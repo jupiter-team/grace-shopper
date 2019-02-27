9ed45aeba09e62fc6ae850829f12a8897250be55
Jia <l19960108@gmail.com>
2019-02-27 16:52:25 -0500

    cart component

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,42 +1,79 @@
 import React, {Component} from 'react'
 
-class Cart extends Component {
-  render() {
-    const cart = {
+const cart = {
+  id: 1,
+  status: 'fulfilled',
+  createdAt: '2019-02-27T21:16:55.084Z',
+  updatedAt: '2019-02-27T21:16:55.084Z',
+  userId: null,
+  guestId: null,
+  orderItems: [
+    {
       id: 1,
-      status: 'open',
-      createdAt: '2019-02-27T17:29:16.499Z',
-      updatedAt: '2019-02-27T17:29:16.499Z',
-      userId: null,
-      guestId: null,
-      orderItems: [
-        {
-          id: 1,
-          quantity: 3,
-          createdAt: '2019-02-27T17:29:16.517Z',
-          updatedAt: '2019-02-27T17:29:16.517Z',
-          productId: 1,
-          orderId: 1
-        },
-        {
-          id: 3,
-          quantity: 1,
-          createdAt: '2019-02-27T17:29:16.517Z',
-          updatedAt: '2019-02-27T17:29:16.517Z',
-          productId: 5,
-          orderId: 1
-        },
-        {
-          id: 2,
-          quantity: 3,
-          createdAt: '2019-02-27T17:29:16.517Z',
-          updatedAt: '2019-02-27T17:29:16.517Z',
-          productId: 1,
-          orderId: 1
-        }
-      ]
+      quantity: 3,
+      createdAt: '2019-02-27T21:16:55.120Z',
+      updatedAt: '2019-02-27T21:16:55.120Z',
+      productId: 1,
+      orderId: 1,
+      product: {
+        id: 1,
+        name: 'Mint Green Tea',
+        price: '8.50',
+        description:
+          'In Morocco and many other Arab countries, serving mint tea has come to represent a certain lifestyle and the most refined expression of hospitality. Green tea is always used, usually Gunpowder, which is known for its refreshing and thirst quenching qualities. Chinese green tea (Gunpowder) rolled into pearls and flavored with dried mint leaves. Astringent, fresh and thirst quenching.',
+        imageUrl:
+          'https://us.palaisdesthes.com/pub/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/8/1/815-42140-idd04ibnlv.jpg',
+        status: 'available',
+        createdAt: '2019-02-27T21:16:54.995Z',
+        updatedAt: '2019-02-27T21:16:54.995Z'
+      }
+    },
+    {
+      id: 2,
+      quantity: 1,
+      createdAt: '2019-02-27T21:16:55.121Z',
+      updatedAt: '2019-02-27T21:16:55.121Z',
+      productId: 5,
+      orderId: 1,
+      product: {
+        id: 5,
+        name: 'Chai Imperial Black Tea',
+        price: '8.50',
+        description:
+          'Loved by our customers, the Chai n°25 recipes becomes a permanent reference! Inspired by the Indian tradition of spiced tea, Chai Impérial is a delicate black tea pepped up with a generous blend of green cardamom, pink peppercorns, cinnamon, ginger, and orange zest. Full of precious spices, Chai Imperial is a delicious and warm blend which can be prepared nature or with milk like in the Indian tradition.',
+        imageUrl:
+          'https://us.palaisdesthes.com/pub/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/7/7/771-34801-v9p8uywm6p.jpg',
+        status: 'available',
+        createdAt: '2019-02-27T21:16:54.998Z',
+        updatedAt: '2019-02-27T21:16:54.998Z'
+      }
+    },
+    {
+      id: 4,
+      quantity: 3,
+      createdAt: '2019-02-27T21:16:55.121Z',
+      updatedAt: '2019-02-27T21:16:55.121Z',
+      productId: 1,
+      orderId: 1,
+      product: {
+        id: 1,
+        name: 'Mint Green Tea',
+        price: '8.50',
+        description:
+          'In Morocco and many other Arab countries, serving mint tea has come to represent a certain lifestyle and the most refined expression of hospitality. Green tea is always used, usually Gunpowder, which is known for its refreshing and thirst quenching qualities. Chinese green tea (Gunpowder) rolled into pearls and flavored with dried mint leaves. Astringent, fresh and thirst quenching.',
+        imageUrl:
+          'https://us.palaisdesthes.com/pub/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/8/1/815-42140-idd04ibnlv.jpg',
+        status: 'available',
+        createdAt: '2019-02-27T21:16:54.995Z',
+        updatedAt: '2019-02-27T21:16:54.995Z'
+      }
     }
+  ]
+}
 
+export class Cart extends Component {
+  render() {
+    console.log('hello world')
     return <div className="cart-page">Welcome To Your Cart!</div>
   }
 }
--- server/api/orders.js
+++ server/api/orders.js
@@ -1,11 +1,13 @@
 const router = require('express').Router()
-const {Order, OrderItem} = require('../db/models')
+const {Order, OrderItem, Product} = require('../db/models')
 module.exports = router
 
 //For logged in users
 router.get('/:orderId', async (req, res, next) => {
   try {
-    const order = await Order.findById(req.params.orderId, {include: OrderItem})
+    const order = await Order.findById(req.params.orderId, {
+      include: [{model: OrderItem, include: [Product]}]
+    })
     res.json(order)
   } catch (error) {
     next(error)
