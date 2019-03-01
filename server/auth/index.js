7cde52c0f7b106179096e56600aae13956e4a249
friggito <ornella.friggit@gmail.com>
2019-03-01 18:08:35 -0500

    feat(login/cart/signup):Set up openOrderAndCart function and added it to signup process

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,6 +1,40 @@
 const router = require('express').Router()
-const User = require('../db/models/user')
+const {User, Order, OrderItem, Product} = require('../db/models/')
 module.exports = router
+
+const openOrderAndCart = async user => {
+  const [openOrder] = await Order.findOrCreate({
+    where: {userId: user.id, status: 'open'}
+  })
+  let orderItems = await OrderItem.findAll({
+    where: {orderId: openOrder.id},
+    include: Product
+  })
+  let destructuredOrderItems = orderItems.map(item => ({
+    product: {
+      id: item.product.id,
+      name: item.product.name,
+      price: item.product.price,
+      description: item.product.description,
+      image: item.product.image,
+      status: item.product.status
+    },
+    quantity: item.quantity,
+    orderId: item.orderId
+  }))
+  if (req.session.cart && req.session.cart.orderItems) {
+    destructuredOrderItems = [
+      ...destructuredOrderItems,
+      req.session.cart.orderItems
+    ]
+  }
+  req.session.cart = {
+    userId: user.id,
+    orderId: openOrder.id,
+    status: openOrder.status,
+    orderItems: [...destructuredOrderItems]
+  }
+}
 
 router.post('/login', async (req, res, next) => {
   try {
@@ -13,8 +47,10 @@ router.post('/login', async (req, res, next) => {
       res.status(401).send('Wrong username and/or password')
     } else {
       req.login(user, err => (err ? next(err) : res.json(user)))
+      openOrderAndCart(user)
     }
   } catch (err) {
+    console.log(err)
     next(err)
   }
 })
@@ -23,6 +59,7 @@ router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
     req.login(user, err => (err ? next(err) : res.json(user)))
+    openOrderAndCart(user)
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists')
