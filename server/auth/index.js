12bc7297f7881c4575445a17b31cef32f2198799
friggito <ornella.friggit@gmail.com>
2019-03-03 12:24:13 -0500

    feat(login/signup/cart) finished setting up session cart at login or signup, fully functional

--- server/auth/index.js
+++ server/auth/index.js
@@ -2,37 +2,42 @@ const router = require('express').Router()
 const {User, Order, OrderItem, Product} = require('../db/models/')
 module.exports = router
 
-const openOrderAndCart = async user => {
-  const [openOrder] = await Order.findOrCreate({
-    where: {userId: user.id, status: 'open'}
-  })
-  let orderItems = await OrderItem.findAll({
-    where: {orderId: openOrder.id},
-    include: Product
-  })
-  let destructuredOrderItems = orderItems.map(item => ({
-    product: {
-      id: item.product.id,
-      name: item.product.name,
-      price: item.product.price,
-      description: item.product.description,
-      image: item.product.image,
-      status: item.product.status
-    },
-    quantity: item.quantity,
-    orderId: item.orderId
-  }))
-  if (req.session.cart && req.session.cart.orderItems) {
-    destructuredOrderItems = [
-      ...destructuredOrderItems,
-      req.session.cart.orderItems
-    ]
-  }
-  req.session.cart = {
-    userId: user.id,
-    orderId: openOrder.id,
-    status: openOrder.status,
-    orderItems: [...destructuredOrderItems]
+const makeOrder = async (user, req, next) => {
+  try {
+    const [openOrder] = await Order.findOrCreate({
+      where: {userId: user.id, status: 'open'}
+    })
+    let orderItems = await OrderItem.findAll({
+      where: {orderId: openOrder.id},
+      include: Product
+    })
+    let destructuredOrderItems = orderItems.map(item => ({
+      product: {
+        id: item.product.id,
+        name: item.product.name,
+        price: item.product.price,
+        description: item.product.description,
+        image: item.product.image,
+        status: item.product.status
+      },
+      quantity: item.quantity,
+      orderId: item.orderId
+    }))
+    if (req.session.cart && req.session.cart.orderItems) {
+      destructuredOrderItems = [
+        ...destructuredOrderItems,
+        req.session.cart.orderItems
+      ]
+    }
+    req.session.cart = {
+      userId: user.id,
+      orderId: openOrder.id,
+      status: openOrder.status,
+      orderItems: [...destructuredOrderItems]
+    }
+    console.log(req.session.cart)
+  } catch (error) {
+    next(err)
   }
 }
 
@@ -47,7 +52,7 @@ router.post('/login', async (req, res, next) => {
       res.status(401).send('Wrong username and/or password')
     } else {
       req.login(user, err => (err ? next(err) : res.json(user)))
-      openOrderAndCart(user)
+      makeOrder(user, req, next)
     }
   } catch (err) {
     console.log(err)
@@ -59,7 +64,7 @@ router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
     req.login(user, err => (err ? next(err) : res.json(user)))
-    openOrderAndCart(user)
+    makeOrder(user, req, next)
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists')
