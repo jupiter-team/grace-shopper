27866281f61aeff7435d7fd3ca5aa68af7379672
Christine Kim <chrismekim@gmail.com>
2019-03-05 19:11:11 -0500

    feat(carousel, home): added homepage carousel

--- client/components/carousel.js
+++ client/components/carousel.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-export const Carousel = () => {
+const Carousel = () => {
   return (
     <div id="home-carousel" className="carousel slide" data-ride="carousel">
       <div className="carousel-inner">
@@ -14,15 +14,9 @@ export const Carousel = () => {
             alt="First slide"
           />
         </div>
-
-        <div className="carousel-item active">
-          <img
-            className="d-block w-100"
-            src="https://cdn.shopify.com/s/files/1/0041/3842/t/16/assets/slideshow_3.jpg?18280894639576543473"
-            alt="Second slide"
-          />
-        </div>
       </div>
     </div>
   )
 }
+
+export default Carousel
